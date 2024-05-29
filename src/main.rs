0714e65207b6b430159ce5a7cca3680d43755369
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-05-29 01:34:22 -0500

    Sonia initial setup

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+
+use dotenv::dotenv;
+
+use serenity::{async_trait, model::channel::Message, prelude::*};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sendind message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating user");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
